f9105d4612369ebbeab2acb0395de8f67dfe0bf3
adga010 <adga1010@gmail.com>
2024-04-21 18:02:42 -0500

    url

--- project_planner/urls.py
+++ project_planner/urls.py
@@ -1,12 +1,17 @@
 # project_planner/urls.py
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+
+
+def root_view(request):
+    return JsonResponse({"message": "Bienvenido a mi API"})
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("usuarios_app.urls")),  # Asegúrate de tener esta línea
-    path("api/", include("proyectos.urls")),  # Asegúrate de tener esta línea
-    path("api/", include("configuracion.urls")),  # Asegúrate de tener esta línea
-
+    path("api/", include("usuarios_app.urls")),
+    path("api/", include("proyectos.urls")),
+    path("api/", include("configuracion.urls")),
+    path("", root_view),  # Agrega esta línea para la ruta raíz
 ]
