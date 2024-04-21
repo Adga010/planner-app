6dc375fd1ab11334751552cfe161cc7fd75144b0
adga010 <adga1010@gmail.com>
2024-04-21 18:12:56 -0500

    settings 2

--- project_planner/settings.py
+++ project_planner/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1$d7wrejjyf^29z=!)+dy#=qn*wee!ikq*rpv_be_f7kihck(8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
@@ -198,7 +198,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
